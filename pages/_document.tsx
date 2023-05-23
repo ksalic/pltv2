90d895db2f31b0426cbd7c9e77fc4f3632e1b223
Kenan Salic <kenan.salic@bloomreach.com>
2023-05-23 14:56:06 +0200

    plt demo personalization

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,46 +8,29 @@ export default function Document() {
                 <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
                       integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p"
                       crossOrigin="anonymous"/>
-                {/*<link rel="preload"*/}
-                {/*      href="https://cdn-skin.prettylittlething.com/use1/_next/static/css/bf0287d035e9c83d.css"*/}
-                {/*      as="style"/>*/}
-                {/*<link rel="stylesheet"*/}
-                {/*      href="https://cdn-skin.prettylittlething.com/use1/_next/static/css/bf0287d035e9c83d.css"*/}
-                {/*      data-n-g=""/>*/}
-                {/*<link rel="preload"*/}
-                {/*      href="https://cdn-skin.prettylittlething.com/use1/_next/static/css/e694871cfad61bfd.css"*/}
-                {/*      as="style"/>*/}
-                {/*<link rel="stylesheet"*/}
-                {/*      href="https://cdn-skin.prettylittlething.com/use1/_next/static/css/e694871cfad61bfd.css"*/}
-                {/*      data-n-p=""/>*/}
-                {/*<link rel="preload"*/}
-                {/*      href="https://cdn-skin.prettylittlething.com/use1/_next/static/css/690858c0f172c7bf.css"*/}
-                {/*      as="style"/>*/}
-                {/*<link rel="stylesheet"*/}
-                {/*      href="https://cdn-skin.prettylittlething.com/use1/_next/static/css/690858c0f172c7bf.css"/>*/}
 
                 <link rel="preload"
-                      href="https://cdn-skin.prettylittlething.com/use1/_next/static/css/4ada1f201a0ddea9.css"
+                      href="/css/4ada1f201a0ddea9.css"
                       as="style"/>
                 <link rel="stylesheet"
-                      href="https://cdn-skin.prettylittlething.com/use1/_next/static/css/4ada1f201a0ddea9.css"
+                      href="/css/4ada1f201a0ddea9.css"
                       data-n-g=""/>
                 <link rel="preload"
-                      href="https://cdn-skin.prettylittlething.com/use1/_next/static/css/5f9e7359887782ed.css"
+                      href="/css/5f9e7359887782ed.css"
                       as="style"/>
                 <link rel="stylesheet"
-                      href="https://cdn-skin.prettylittlething.com/use1/_next/static/css/5f9e7359887782ed.css"
+                      href="/css/5f9e7359887782ed.css"
                       data-n-p=""/>
                 <link rel="preload"
-                      href="https://cdn-skin.prettylittlething.com/use1/_next/static/css/e46435daad24b769.css"
+                      href="/css/e46435daad24b769.css"
                       as="style"/>
                 <link rel="stylesheet"
-                      href="https://cdn-skin.prettylittlething.com/use1/_next/static/css/e46435daad24b769.css"/>
+                      href="/css/e46435daad24b769.css"/>
                 <link rel="preload"
-                      href="https://cdn-skin.prettylittlething.com/use1/_next/static/css/690858c0f172c7bf.css"
+                      href="/css/690858c0f172c7bf.css"
                       as="style"/>
                 <link rel="stylesheet"
-                      href="https://cdn-skin.prettylittlething.com/use1/_next/static/css/690858c0f172c7bf.css"/>
+                      href="/css/690858c0f172c7bf.css"/>
 
             </Head>
             <body>
